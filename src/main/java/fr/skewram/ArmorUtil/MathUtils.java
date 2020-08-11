b71fd9485615b54d6a1d925537a0f4d593e5abb9
Noan <noan.perrot@gmail.com>
2020-08-11 16:17:33 +0200

    Optimize degToRad

--- src/main/java/fr/skewram/ArmorUtil/MathUtils.java
+++ src/main/java/fr/skewram/ArmorUtil/MathUtils.java
@@ -2,6 +2,6 @@ package fr.skewram.ArmorUtil;
 
 public class MathUtils {
     static double degToRad(double degree){
-        return degree * (Math.PI / 2) / 90.0;
+        return degree * Math.PI / 180;
     }
 }
